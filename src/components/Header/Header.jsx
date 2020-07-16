7f58a1c255fb5969b34cfdc3a5a3cb725d61e03c
NayamaRosa <falecom@nayama.com.br>
2020-07-16 12:48:38 -0300

    Header e header logger

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header = (props) => {
+  return (
+    <header className="align">
+      <h2 className="text-initial-header">SISTEMA INTERNO DE PEDIDOS</h2>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/HeaderLogged/HeaderLogged.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import firebase from '../firebaseConfig';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import Button from '../Button';
+import Logo from '../Logo';
+
+import './HeaderLogged.scss';
+
+const firebaseAppAuth = firebase.auth();
+
+class HeaderLogged extends React.Component{
+  
+  signOut = () => {
+    firebaseAppAuth.signOut()
+    .then(() => {
+      sessionStorage.clear();
+      window.location = '/';
+    })
+  };
+  
+  render (){
+    return (
+      <header className="align-dashboard-header">
+        <p className="person-name">{sessionStorage.getItem('name')}</p>
+        <Logo className="logo-dashboard-header"/>
+        <Button className="sign-out" text="Sair" onClick={this.signOut} />
+      </header>
+    );
+  }
+  
+}
+
+export default withFirebaseAuth({firebaseAppAuth,})(HeaderLogged);
