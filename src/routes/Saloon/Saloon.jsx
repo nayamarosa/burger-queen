4bb4e4c13b7d7e1fb40c190c660c876a3b7fdcff
NayamaRosa <falecom@nayama.com.br>
2020-07-17 19:19:31 -0300

    caminhos de renderização no asideMenu ok

--- src/routes/Saloon/Saloon.jsx
+++ src/routes/Saloon/Saloon.jsx
@@ -1,29 +1,84 @@
-import React from 'react';
-// import Breakfast from '../components/Breakfast'
-import AsideMenu from '../../components/AsideMenu';
-// import Order from '../components/Order';
-// import '../components/Components.css';
+import React, { useContext, useState, useEffect } from 'react';
 
-function Saloon() {
+import { MenuContext } from '../../containers/Context';
+import HeaderLogged from '../../components/HeaderLogged';
+import AsideMenu from '../../components/AsideMenu';
+import BreakfastList from '../../containers/BreakfastList';
+import AlldayList from '../../containers/AlldayList';
+
+const Saloon = () => {
+  const menu = useContext(MenuContext);
+  const [active, setActive] = useState(null);
+  const [menuBreakfastList, setMenuBreakfastList] = useState([]);
+  const [menuAlldayList, setMenuAlldayList] = useState([]);
+
+  useEffect(() => {
+    let breakfastList = menu.filter(item =>  
+      item.breakfast
+    )
+    setMenuBreakfastList(breakfastList)
+
+    let alldayList = menu.filter(item =>  
+      item.allday
+    )
+    setMenuAlldayList(alldayList)
+  }, [menu]);
   
+  const handleMenuFilter = (e) => {
+    e.preventDefault();
+    let activeFilter = e.target;
+    activeFilter.classList.add("classe");
+    setActive(e.target);
+    if (active !== e.target) {
+      setActive(e.target);
+    }
+    if (active) {
+      active.classList.remove("classe");
+    }
+  }
+
+  const renderSwitch = (param) => {
+    switch(param !== null) {
+      case param.textContent === "Café da manhã":
+        return <BreakfastList menu={menuBreakfastList}/>;
+      case param.textContent === "Almoço e Jantar":
+        return <AlldayList menu={menuAlldayList}/>;
+      case param.textContent === "Pedidos prontos":
+        return param.textContent;
+      case param.textContent === "Meus pedidos":
+        return param.textContent;
+      default:
+        return <p>Selecione uma opção</p>;
+    }
+  }
+
   return (
     <>
+    <HeaderLogged />
     <AsideMenu 
       firstIcon="fas fa-coffee"
       textFirstButton="Café da manhã"
+      firstClick={(e) => handleMenuFilter(e)}
       
       secondIcon="fas fa-hamburger"
       textSecondButton="Almoço e Jantar"
-
+      secondClick={(e) => handleMenuFilter(e)}
+      
       thirdIcon="fas fa-utensils"
       textThirdButton="Pedidos prontos"
+      thirdClick={(e) => handleMenuFilter(e)}
       
       fourthIcon="fas fa-clipboard-list"
       textFourthButton="Meus pedidos"
+      fourthClick={(e) => handleMenuFilter(e)}
     />
-    {/* <GetOrder/> */}
+    { 
+    active !== null
+      ? renderSwitch(active)
+      : <p>Selecione uma opção</p>
+    }
     </>
-    )
-  }
-  
-  export default Saloon;
+  )
+}
+
+export default Saloon;
